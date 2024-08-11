bbd828307c1144239e030fa488bef66ab2c52c66
DeadMor0z <13347404+DeadMor0z@users.noreply.github.com>
2024-08-12 00:40:33 +0300

    * update for game version 0.26.0

--- src/main/java/biomeclutter/listener/GeneratedIslandFloraListener.java
+++ src/main/java/biomeclutter/listener/GeneratedIslandFloraListener.java
@@ -25,7 +25,7 @@ public class GeneratedIslandFloraListener extends GameEventListener<GeneratedIsl
             event.islandGeneration.generateObjects(ObjectRegistry.getObjectID("fallenpinetrunk"), TileRegistry.snowID, GameRandom.globalRandom.getFloatBetween(0.001F, 0.003F));
             event.islandGeneration.generateObjects(ObjectRegistry.getObjectID("cutpinetrunk"), TileRegistry.snowID, GameRandom.globalRandom.getFloatBetween(0.0002F, 0.0005F));
 
-            event.islandGeneration.generateFruitGrowerVeins("sweetberrybush", 0.04F, 8, 10, 0.1F, TileRegistry.snowID);
+            event.islandGeneration.generateFruitGrowerVeins("sweetberrybush", 0.04F, 8, 10, 0.1F, null, new int[] { TileRegistry.snowID });
 
         } else if (event.level instanceof SwampSurfaceLevel) {
             event.islandGeneration.generateObjects(ObjectRegistry.getObjectID("fallenwillowtrunk"), TileRegistry.swampGrassID, GameRandom.globalRandom.getFloatBetween(0.001F, 0.003F));
